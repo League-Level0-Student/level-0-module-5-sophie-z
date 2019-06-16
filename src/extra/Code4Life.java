77e0fdf2c9cfd74d6a966619f5967ee902f6c7cb
League Student <league@WTS9.attlocal.net>
2019-06-16 14:26:46 -0700

    SkillPractice, Code4Life

--- src/extra/Code4Life.java
+++ src/extra/Code4Life.java
@@ -11,19 +11,28 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 public class Code4Life {
-
+public static void main(String[] args) {
 	// 1. Ask the user how many hours they spent coding this week.
-
+String hours = JOptionPane.showInputDialog("How many hours did you spend coding this week?");
+int h = Integer.parseInt(hours);
 	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
 	// write code instead.
-
+if(h<=2) {
+	JOptionPane.showMessageDialog(null, "STOP WATCHING YOUTUBE AND CODE INSTEAD!");
+}
 	// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
 	// they're a Code Ninja
-
+if(h>=3 && h<=-5) {
+	JOptionPane.showMessageDialog(null, "Congratulations! You're a code ninja!");
+}
 	// 4. If it is more than 5, call the method below to play the Batman theme song.
-
+if(h>5) {
+	playBatmanTheme();
+}
+}
 	private static void playBatmanTheme() {
 		try {
 			AudioClip sound = JApplet.newAudioClip(Code4Life.class.getResource("batman.wav"));
--- src/nested_loops/SkillPractice.java
+++ src/nested_loops/SkillPractice.java
@@ -1,5 +1,7 @@
 package nested_loops;
 
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
 public class SkillPractice {
@@ -32,57 +34,51 @@ else {
 //If they are shorter than 36 inches, tell them to eat their Wheaties
 void skill2() { 
 // Write a loop to print every third number between 1 and 30 to the console 
-for(int i=1; i<31; i++) {
+for(int i=2; i<30; i++) {
 System.out.println(i);
 }
 }
 void skill3() { // Get a random number that is less than 20 and print it to the console 
-
-
-
+int r = new Random().nextInt(20);
+System.out.println(r);
 //Get another random number that is less than 10 and print it to the console 
-
-
-
+int random = new Random().nextInt(10);
+System.out.println(random);
+int s = r-random;
+JOptionPane.showMessageDialog(null, "The difference between the two random numbers is " + s);
 //Using a pop-up, tell the user the difference between the numbers // Hint: use subtraction 
-
-
-
 }
 
 void skill4() { // In a pop-up, ask the user for the city they live in 
-
-
-
+String city = JOptionPane.showInputDialog("What city do you live in?");
+if(city.equals("San Diego")) {
+JOptionPane.showMessageDialog(null, "Congratulations. You live in America's finest city!");
+}
+else {
+JOptionPane.showMessageDialog(null, "Incorrect answer. MOVE TO SAN DIEGO!");
+}
 //If they answered "San Diego", tell them they live in America's Finest City 
-
-
-
 //Otherwise, tell them to move to San Diego 
-
-
-
 //Create a variable - cars - and initialize it to the number of cars your family has. // If there are 0 cars, use a pop-up to display, "I bet you use public transportation." 
-
-
-
+String cars = null;
+int c = Integer.parseInt(cars);
+c = 3;
+if(c==0) {
+JOptionPane.showMessageDialog(null, "HA! I bet you use public transportation!");
+}
+if(c==1) {
+JOptionPane.showMessageDialog(null, "White Acura");
+}
+if(c>1) {
+JOptionPane.showMessageDialog(null, "12  wheels");
+}
 //If there is 1 car, use a pop-up to display the make/model of the car 
-
-
-
 //If there is more than 1 car, use a pop-up to display how many wheels the // cars have between them. 
-
-
-
 }
 
 void skill5() { // In a pop-up, ask the user for the name of their school 
-
-
-
+String school = JOptionPane.showInputDialog("What school do you go to?");
+JOptionPane.showMessageDialog(null, school + " is a fantastic school.");
 //In another pop-up, tell the user, that their school is a fantastic school. // You must include the name of the school in the message. 
-
-
-
 }
 }
